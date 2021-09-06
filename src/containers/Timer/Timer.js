403564c43007d17b076861b30a3160f64f115d96
Serg-Ul <sergit001k@gmail.com>
2021-09-07 00:41:43 +0300

    updated Timer

--- src/containers/Timer/Timer.js
+++ src/containers/Timer/Timer.js
@@ -3,23 +3,26 @@ import Layout from "../../hoc/Layout/Layout";
 import classes from "./Timer.module.css";
 //import timer from "../../images/timer.png"
 import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
 
 class Timer extends React.Component {
     state = {
-        defaultTime: null,
+        defaultTime: '0:0:0',
+        defaultValues: {
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        },
         formControls: {
             hours: {
-                defaultHours: 0,
                 label: 'Hours',
                 value: 0,
             },
             minutes: {
-                defaultMinutes: 0,
                 label: 'Minutes',
                 value: 0,
             },
             seconds: {
-                defaultSeconds: 0,
                 label: 'Seconds',
                 value: 0,
             }
@@ -27,39 +30,42 @@ class Timer extends React.Component {
     }
 
     getTime = (control) => {
-        const date = new Date();
+        const defaultValues = {...this.state.defaultValues}
 
         switch (control.label.toLowerCase()) {
             case 'hours': {
-                control.defaultHours = control.value;
+                defaultValues.hours = +control.value;
                 break;
             }
             case 'minutes': {
-                control.defaultMinutes = control.value;
+                defaultValues.minutes = +control.value;
                 break;
             }
             case 'seconds': {
-                control.defaultSeconds = control.value;
+                defaultValues.seconds = +control.value;
+                break;
+            }
+            default : {
+                return;
             }
         }
 
-        console.log(control);
+        this.setState({
+            defaultValues
+        })
 
-        date.setHours(0, 0, 0);
-
-        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        return `${defaultValues.hours}:${defaultValues.minutes}:${defaultValues.seconds}`;
     }
 
     onChangeInput = (value, controlName) => {
-        const defaultTime = this.state.defaultTime;
+        //const defaultTime = this.state.defaultTime;
         const formControls = {...this.state.formControls}
         const control = formControls[controlName];
 
         // Set value in control
         control.value = value;
 
-        const time = this.getTime(control);
-        console.log(time);
+        const defaultTime = this.getTime(control);
 
         // Rewrite control
         formControls[controlName] = control;
@@ -67,7 +73,7 @@ class Timer extends React.Component {
         // Set state
         this.setState({
             formControls,
-            defaultTime: time
+            defaultTime
         })
     }
 
@@ -90,6 +96,69 @@ class Timer extends React.Component {
         })
     }
 
+    onChangeDefaultTime = () => {
+        const defaultValues = {...this.state.defaultValues};
+
+        const interval = setInterval(() => {
+            defaultValues.seconds = defaultValues.seconds ? defaultValues.seconds - 1 : 1 - 1;
+
+            if (defaultValues.seconds === 0 && defaultValues.minutes) {
+                defaultValues.seconds = 59;
+                defaultValues.minutes = defaultValues.minutes - 1;
+
+                if (defaultValues.minutes === 0 && defaultValues.hours) {
+                    defaultValues.minutes = 59;
+                    defaultValues.hours = defaultValues.hours - 1;
+                }
+            }
+
+            this.setState({
+                defaultTime: `${defaultValues.hours}:${defaultValues.minutes}:${defaultValues.seconds}`,
+                defaultValues
+            })
+
+            if (this.state.defaultTime === '0:0:0') {
+                clearInterval(interval);
+            }
+        }, 300)
+        // const timeLeft = (func, ms) => new Promise(resolve => {
+        //     const intervalLeft = setInterval(func, ms);
+        //     resolve(intervalLeft);
+        // });
+
+        // const interval = await timeLeft(() => {
+        //     defaultValues.seconds = +defaultValues.seconds - 1;
+        //
+        //     if (defaultValues.seconds === 0 && defaultValues.minutes) {
+        //         defaultValues.seconds = 59;
+        //         defaultValues.minutes = defaultValues.minutes - 1;
+        //
+        //         if (defaultValues.minutes === 0 && defaultValues.hours) {
+        //             defaultValues.minutes = 59;
+        //             defaultValues.hours = defaultValues.hours - 1;
+        //         }
+        //     }
+        //
+        //     this.setState({
+        //         defaultTime: `${defaultValues.hours}:${defaultValues.minutes}:${defaultValues.seconds}`,
+        //         defaultValues
+        //     })
+        // }, 1000)
+        //
+        // setTimeout(() => {
+        //     clearInterval(interval)
+        // }, (defaultValues.seconds * 1000) + (defaultValues.minutes * 60000) + (defaultValues.hours * 600000))
+        // defaultValues.seconds * 1000)
+
+        // <= or =>
+        // .then(interval => {
+        //     setTimeout(() => {
+        //         clearInterval(interval)
+        //     }, defaultValues.seconds * 1000)
+        // })
+
+    }
+
     render() {
         // const styles = {
         //     backgroundImage: `url(${timer})`
@@ -101,6 +170,9 @@ class Timer extends React.Component {
                         <div>{this.state.defaultTime}</div>
                     </div>
                     {this.renderInputs()}
+                    <Button type="success" onClick={this.onChangeDefaultTime}>
+                        Starting timer
+                    </Button>
                 </div>
             </Layout>
         )
